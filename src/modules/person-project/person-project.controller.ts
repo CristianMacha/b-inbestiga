e578e1d71447cccc8f27c431d905f2acdc7a6898
Alejandro Garcia <alejandro@horsesdeveloper.com>
2022-04-10 19:07:10 -0500

    add crud person project

--- src/modules/person-project/person-project.controller.ts
+++ src/modules/person-project/person-project.controller.ts
@@ -1,4 +1,23 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { PersonProject } from './person-project.entity';
+import { PersonProjectService } from './person-project.service';
 
 @Controller('person-project')
-export class PersonProjectController {}
+export class PersonProjectController {
+    constructor(private personProjectServices: PersonProjectService) {}
+
+    @Post()
+    async create(@Body() personProject: PersonProject): Promise<PersonProject> {
+        return await this.personProjectServices.create(personProject);
+    } 
+
+    @Get()
+    async findAll(): Promise<PersonProject[]> {
+        return await this.personProjectServices.findAll();
+    }
+
+    @Get(':id')
+    async findOne(@Param('id') id: string): Promise<PersonProject> {
+        return await this.personProjectServices.findOne(+id);
+    }
+}
--- src/modules/person-project/person-project.service.ts
+++ src/modules/person-project/person-project.service.ts
@@ -1,4 +1,49 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { PersonProject } from './person-project.entity';
+import { PersonProjectRepository } from './person-project.repository';
 
 @Injectable()
-export class PersonProjectService {}
+export class PersonProjectService {
+  constructor(private personProjectRepository: PersonProjectRepository) {}
+
+  async create(personProject: PersonProject): Promise<PersonProject> {
+    try {
+      const newPersonProject =
+        this.personProjectRepository.create(personProject);
+      const newPersonProjectCreated = await this.personProjectRepository.save(
+        newPersonProject,
+      );
+
+      return newPersonProjectCreated;
+    } catch (error) {
+      throw new BadRequestException(error);
+    }
+  }
+
+  async findAll(): Promise<PersonProject[]> {
+    try {
+      const listPersonProject = await this.personProjectRepository.find({
+        relations: ['person', 'project'],
+        where: { active: true },
+      });
+
+      return listPersonProject;
+    } catch (error) {
+      throw new BadRequestException(error);
+    }
+  }
+
+  async findOne(personProjectId: number): Promise<PersonProject> {
+    try {
+      const personProjectDb = await this.personProjectRepository.findOne(
+        personProjectId,
+        {
+          relations: ['person', 'project'],
+        },
+      );
+      return personProjectDb;
+    } catch (error) {
+      throw new BadRequestException(error);
+    }
+  }
+}
