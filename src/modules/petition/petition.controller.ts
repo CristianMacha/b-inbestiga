fe88206d16c92096e67440288430f6f745b9a779
Alejandro Garcia <alejandro@horsesdeveloper.com>
2022-04-16 14:40:16 -0500

    add petitions

--- src/modules/petition/petition.controller.ts
+++ src/modules/petition/petition.controller.ts
@@ -1,4 +1,23 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { Petition } from './petition.entity';
+import { PetitionService } from './petition.service';
 
 @Controller('petition')
-export class PetitionController {}
+export class PetitionController {
+    constructor(private petitionServices: PetitionService) {}
+
+    @Post()
+    async create(@Body() petition: Petition): Promise<Petition> {
+        return await this.petitionServices.create(petition);
+    }
+
+    @Get()
+    async findAll(): Promise<Petition[]> {
+        return await this.petitionServices.findAll();
+    }
+
+    @Get(':petitionId')
+    async findOne(@Param('petitionId') petitionId: string): Promise<Petition> {
+        return await this.petitionServices.findOne(+petitionId);
+    }
+}
--- src/modules/petition/petition.service.ts
+++ src/modules/petition/petition.service.ts
@@ -1,4 +1,36 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { Petition } from './petition.entity';
+import { PetitionRepository } from './petition.repository';
 
 @Injectable()
-export class PetitionService {}
+export class PetitionService {
+    constructor(private petitionRepository: PetitionRepository) {}
+
+    async create(petition: Petition): Promise<Petition> {
+        try {
+            const newPetition = this.petitionRepository.create(petition);
+            const newPetitionCreated = await this.petitionRepository.save(newPetition);
+            return newPetitionCreated;
+        } catch (error) {
+            throw new BadRequestException(error);
+        }
+    }
+
+    async findAll(): Promise<Petition[]> {
+        try {
+            const listPetition = await this.petitionRepository.find({ where: { active: true } });
+            return listPetition;
+        } catch (error) {
+            throw new BadRequestException(error);
+        }
+    }
+
+    async findOne(petitionId: number): Promise<Petition> {
+        try {
+            const petitionDb = await this.petitionRepository.findOne(petitionId);
+            return petitionDb;
+        } catch (error) {
+            throw new BadRequestException(error);
+        }
+    }
+}
