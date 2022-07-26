f1d8c98d927da18dd957599048f95a4e7364150f
cistianvms <c71917455@gmail.com>
2022-07-26 11:41:15 -0500

    unique email

--- src/modules/petition/petition.entity.ts
+++ src/modules/petition/petition.entity.ts
@@ -1,59 +1,59 @@
 import {
-  Column,
-  CreateDateColumn,
-  Entity,
-  ManyToOne,
-  PrimaryGeneratedColumn,
-  UpdateDateColumn,
+    Column,
+    CreateDateColumn,
+    Entity,
+    ManyToOne,
+    PrimaryGeneratedColumn,
+    UpdateDateColumn,
 } from 'typeorm';
-import { Category } from '../category/category.entity';
-import { EPetitionStatus } from './enum/petition-status.enum';
+import {Category} from '../category/category.entity';
+import {EPetitionStatus} from './enum/petition-status.enum';
 
 @Entity()
 export class Petition {
-  @PrimaryGeneratedColumn()
-  id: number;
+    @PrimaryGeneratedColumn()
+    id: number;
 
-  @Column()
-  name: string;
+    @Column()
+    name: string;
 
-  @Column()
-  surnames: string;
+    @Column()
+    surnames: string;
 
-  @Column()
-  email: string;
+    @Column({unique: true})
+    email: string;
 
-  @Column()
-  phone: string;
+    @Column()
+    phone: string;
 
-  @Column({
-    type: 'enum',
-    enum: EPetitionStatus,
-    default: EPetitionStatus.PENDING,
-  })
-  status: EPetitionStatus;
+    @Column({
+        type: 'enum',
+        enum: EPetitionStatus,
+        default: EPetitionStatus.PENDING,
+    })
+    status: EPetitionStatus;
 
-  @Column()
-  fileName: string;
+    @Column()
+    fileName: string;
 
-  @Column()
-  url: string;
+    @Column()
+    url: string;
 
-  @Column()
-  message: string;
+    @Column()
+    message: string;
 
-  @Column({ default: true })
-  active: boolean;
+    @Column({default: true})
+    active: boolean;
 
-  @Column({ nullable: false, default: false })
-  accepted: boolean;
+    @Column({nullable: false, default: false})
+    accepted: boolean;
 
-  @CreateDateColumn()
-  createdAt: Date;
+    @CreateDateColumn()
+    createdAt: Date;
 
-  @UpdateDateColumn()
-  updatedAt: Date;
+    @UpdateDateColumn()
+    updatedAt: Date;
 
-  @ManyToOne(() => Category, (category) => category.petitions)
-  category: Category;
+    @ManyToOne(() => Category, (category) => category.petitions)
+    category: Category;
 }
